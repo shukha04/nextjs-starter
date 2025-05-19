7bd1c2893df2bc9f337294179927b656c5c6ab8a
Shukhrat Rustamov <shukhrat.rustamov04@gmail.com>
2025-05-19 12:15:48 -0400

    Updated packages, changed postcss config

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+*Updated on 05/19/2025*
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 import type { Config } from "tailwindcss";
 
-export default {
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
-  plugins: [],
+  theme: {
+    extend: {},
+  },
 } satisfies Config;
